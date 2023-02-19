9e9da6aa60dc5837abda28696e609cf342995fc3
Armir <armir.keta@gmail.com>
2023-02-19 14:19:50 +0100

    Init project

---
+++ CateringOrders/CateringOrders/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CateringOrders.Data.Entities;
+
+public class ApplicationUser : IdentityUser<int>
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime BirthDate { get; set; }
+    public string DocumentIdNumber { get; set; } = null!;
+    public DateTime RegisteredDate { get; set; }
+    public DateTime LastLogin { get; set; }
+    public bool IsDeleted { get; set; }
+}

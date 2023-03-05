1a139b62c0e0a31936e5a42d78b065672ab289c4
Armir Keta <armir.keta@gmail.com>
2023-03-06 00:29:14 +0100

    Added Ordes CRUD + changed a few UI in Home Page

---
+++ CateringOrders/CateringOrders/Areas/Identity/Pages/EmployeeOrders/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CateringOrders.Data;
+using CateringOrders.Data.Entities;
+
+namespace CateringOrders.Areas.Identity.Pages.EmployeeOrders;
+
+public class DetailsModel : PageModel
+{
+    private readonly CateringOrders.Data.ApplicationDbContext _context;
+
+    public DetailsModel(CateringOrders.Data.ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+  public Orders Orders { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null || _context.Orders == null)
+        {
+            return NotFound();
+        }
+
+        var orders = await _context.Orders.FirstOrDefaultAsync(m => m.Id == id);
+        if (orders == null)
+        {
+            return NotFound();
+        }
+        else 
+        {
+            Orders = orders;
+        }
+        return Page();
+    }
+}
